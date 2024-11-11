3bdc7198e4ad545b64df7e26c2565574f9ba4bf3
DABrianC <DABrianC@users.noreply.github.com>
2024-11-11 17:26:36 -0500

    BC

---
+++ running script.R
@@ -0,0 +1,32 @@
+
+# November 11, 2024
+
+library(tidyverse)
+library(gapminder)
+
+
+df <- gapminder
+
+head(df)
+
+tail(df)
+
+#I grouped data by continent and year
+# and I calculated the avg gdp per cap
+df1 <- df |> 
+  group_by(continent, year) |> 
+  summarize(avg_gdpPercap = mean(gdpPercap))
+
+head(df1)
+
+tail(df1)
+
+ggplot(df1) +
+  geom_line(aes(x = year, y = avg_gdpPercap, color = continent))
+
+
+
+
+
+
+
